c21911992b6f49da7769d0fe8791e0c1056afa49
huynhduchochtmlcss <huynhducnguyenht@gmail.com>
2025-03-14 19:37:55 +0700

    init project

---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/greeting',function(){
+    return 'Hello world';
+})->name('greeting'); //name of route
+
+Route::get('/greeting/{name}',function($name){
+    return 'Hello' . $name;
+});
+Route::get('/hiU',function(){
+    return redirect('/greeting');
+});
+
+Route::get('/hi2',function(){
+    return redirect()->route('greeting'); //redirect to route hass name 'greeting'
+});
+
+Route::get('/Home', function(){
+    return view('welcome');
+});
