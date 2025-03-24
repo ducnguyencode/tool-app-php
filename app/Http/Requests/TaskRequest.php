ded9642d88c3df87d98cc173588ceaf038fb6f75
huynhduchochtmlcss <136477566+huynhduchochtmlcss@users.noreply.github.com>
2025-03-24 19:55:51 +0700

    session-6-edit

---
+++ app/Http/Requests/TaskRequest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class TaskRequest extends FormRequest{
+
+    public function authorize(){
+        return true; //ai cung vo dc
+    }
+
+    public function rules(){
+        return [
+            'title'=> 'required|max:255',
+            'description'=>'required|min:3|max:255',
+            'long_description'=>'required|min:3|max:255',
+        ];
+    }
+}
--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.app')
-@section('title', 'Create Task')
+@section('title', 'Update Task')
 @section('style')
     .alert-danger {
         color:red;
@@ -7,23 +7,24 @@
 @endsection
 @section('content')
 
-    <form action="{{route('tasks.store')}}" method="POST">
+    <form action="{{ route('tasks.update',['id'=>$task->id]) }}" method="POST">
         @csrf
+        @method('PUT')
         <label for="title">Title</label>
-        <input type="text" name="title" id="title" value="{{old('title')}}">
+        <input type="text" name="title" id="title" value="{{$task->title}}">
         @error('title')
             <div class="alert alert-danger">{{ $message }}</div>
         @enderror
         <label for="description">Description</label>
-        <input type="text" name="description" id="description" value="{{old('description')}}">
+        <input type="text" name="description" id="description" value="{{$task->description}}">
         @error('description')
             <div class="alert alert-danger">{{ $message }}</div>
         @enderror
         <label for="long_description">Long Description</label>
-        <input type="text" name="long_description" id="long_description" value="{{old('long_description')}}">
+        <input type="text" name="long_description" id="long_description" value="{{$task->long_description}}">
         @error('long_description')
             <div class="alert alert-danger">{{ $message }}</div>
         @enderror
-        <button type="submit">Create</button>
+        <button type="submit">Update</button>
     </form>
 @endsection
