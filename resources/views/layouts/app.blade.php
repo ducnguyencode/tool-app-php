d18ef58b2e7747995ac6f9129c318c9f72f10ccc
huynhduchochtmlcss <136477566+huynhduchochtmlcss@users.noreply.github.com>
2025-03-24 18:26:06 +0700

    session-5-form

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,9 +5,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title')</title>
+    <style>
+        @yield('style')
+        .alert-success {
+            color:green;
+        }
+    </style>
 </head>
 <body>
     <h1>@yield('title')</h1>
+    @if(session()->has('success'))
+        <div class="aler alert-success">{{ session()->get('success') }}</div>
+    @endif
     @yield('content')
 </body>
 </html>
