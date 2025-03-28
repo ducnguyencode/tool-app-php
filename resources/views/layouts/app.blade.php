09d9fdb6172a8ebad5006b6a53f137cbde9525ce
huynhduchochtmlcss <136477566+huynhduchochtmlcss@users.noreply.github.com>
2025-03-28 18:08:10 +0700

    session-7-refactor-blade

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title')</title>
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
     <style>
         @yield('style')
         .alert-success {
